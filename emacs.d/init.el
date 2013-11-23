1f2b823ecc3b02f3e539457767db1f713600f464
Bob Nadler <bnadlerjr@gmail.com>
2013-11-23 11:20:14 -0500

    EMACS!

---
+++ emacs.d/init.el
@@ -0,0 +1,15 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+
+(defvar my-packages '(starter-kit
+		      starter-kit-lisp
+		      starter-kit-eshell
+		      clojure-mode
+		      clojure-test-mode
+		      cider))
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
