98b1bc9f1623a802040274722ab4825d7de304c6
Bob Nadler <bnadlerjr@gmail.com>
2013-12-01 15:48:02 -0500

    Switch to MELPA for package management.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/"))
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
 (package-initialize)
 
 (defvar my-packages '(starter-kit
