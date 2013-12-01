dbf5de83124c45f16da93d3d43db5238b3431513
Bob Nadler <bnadlerjr@gmail.com>
2013-12-01 15:48:17 -0500

    Use solarized color theme.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,12 +10,16 @@
 		      clojure-mode
 		      clojure-test-mode
 		      cider
+                      color-theme-solarized
                       markdown-mode
                       window-number))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
+
+;; use solarized light color theme
+(load-theme 'solarized-light t)
 
 ;; paredit.el
 '(define-key paredit-mode-map (kbd "C-S-<left>") 'paredit-backward-slurp-sexp)
@@ -30,3 +34,15 @@
 
 (require 'window-number)
 (window-number-meta-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
