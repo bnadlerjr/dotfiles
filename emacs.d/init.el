49aec1efb821c45beaf6099485db5d85f40e2d7b
Bob Nadler <bnadlerjr@gmail.com>
2013-12-01 15:59:51 -0500

    Emacs: use a better solarized theme package.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,7 +10,7 @@
 		      clojure-mode
 		      clojure-test-mode
 		      cider
-                      color-theme-solarized
+                      solarized-theme
                       markdown-mode
                       window-number))
 
@@ -34,15 +34,3 @@
 
 (require 'window-number)
 (window-number-meta-mode)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
