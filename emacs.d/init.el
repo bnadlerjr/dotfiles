785fbab4cdeabd055c83aa23d6f107d644622ed3
Bob Nadler <bnadlerjr@gmail.com>
2013-12-01 20:31:20 -0500

    Emacs: Add plugins for Ruby and Python development.

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -7,13 +7,18 @@
 (defvar my-packages '(starter-kit
 		      starter-kit-lisp
 		      starter-kit-eshell
+                      auto-complete
 		      clojure-mode
+                      clojure-snippets
 		      clojure-test-mode
 		      cider
+                      jedi
                       solarized-theme
                       markdown-mode
                       powerline
-                      window-number))
+                      smartparens
+                      window-number
+                      yasnippet))
 
 (dolist (p my-packages)
   (when (not (package-installed-p p))
@@ -35,6 +40,26 @@
 (set-face-attribute 'mode-line-inactive nil
                     :box nil)
 
+;; yasnippet
+(require 'yasnippet)
+(setq yas-snippet-dirs
+      '("~/bin/yasnippet-snippets"))
+
+(yas-global-mode 1)
+
+;; auto-complete; must be activated *after* yasnippet plugin
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; set the trigger key so that it can work together with yasnippet on
+;; tab key, if the word exists in yasnippet, pressing tab will cause yasnippet
+;; to activate, otherwise, auto-complete will
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
+
+;; smartparens
+(smartparens-global-mode t)
+
 ;; paredit.el
 '(define-key paredit-mode-map (kbd "C-S-<left>") 'paredit-backward-slurp-sexp)
 '(define-key paredit-mode-map (kbd "C-S-<right>") 'paredit-backward-barf-sexp)
@@ -45,6 +70,10 @@
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
 
 (require 'window-number)
 (window-number-meta-mode)
