b47abec616453963437ab3091d2c05cad4fff347
Bob Nadler <bnadlerjr@gmail.com>
2015-02-23 17:18:50 -0500

    Lein: user namespace helpers.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,2 +1,5 @@
-{:user {:plugins [[cider/cider-nrepl "0.8.2"]]
-        :dependencies [[slamhound "1.3.1"]]}}
+{:user {:dependencies [[org.clojure/tools.namespace  "0.2.9"]
+                       [slamhound "1.5.5"]]
+        :plugins [[cider/cider-nrepl "0.8.2"]]
+        :repl-options  {:init-ns user}
+        :source-paths  [#=(eval (str (System/getProperty "user.home") "/.lein"))]}}
---
+++ lein/user.clj
@@ -0,0 +1,6 @@
+(ns user
+  (:require
+    [clojure.pprint :refer [pprint]]
+    [clojure.repl :refer  [doc find-doc source]]
+    [clojure.test :refer [run-tests run-all-tests]]
+    [clojure.tools.namespace.repl :refer [refresh refresh-all]]))
