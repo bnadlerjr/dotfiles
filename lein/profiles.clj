c78d8f6ac030c9a539ad2f90eaff84749bb015fe
Bob Nadler <bnadlerjr@gmail.com>
2015-03-01 08:42:25 -0500

    LEIN: Remove user profile and REPL code.
    These should be specified in individual projects, not at the global user level.

--- lein/profiles.clj
+++
@@ -1,5 +0,0 @@
-{:user {:dependencies [[org.clojure/tools.namespace  "0.2.9"]
-                       [slamhound "1.5.5"]]
-        :plugins [[cider/cider-nrepl "0.8.2"]]
-        :repl-options  {:init-ns user}
-        :source-paths  [#=(eval (str (System/getProperty "user.home") "/.lein"))]}}
--- lein/user.clj
+++
@@ -1,6 +0,0 @@
-(ns user
-  (:require
-    [clojure.pprint :refer [pprint]]
-    [clojure.repl :refer  [doc find-doc source]]
-    [clojure.test :refer [run-tests run-all-tests]]
-    [clojure.tools.namespace.repl :refer [refresh refresh-all]]))
