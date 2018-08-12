c22edadadbb63793e3c0e77efb842d11fb64fc4f
Bob Nadler <bnadlerjr@gmail.com>
2018-08-12 13:09:53 -0400

    Lein: Remove Ultra plugin
    The plugin was causing issues w/ lein-ring I think

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-try "0.4.3"]
-                   [venantius/ultra "0.5.2"]]
+                   ; [venantius/ultra "0.5.2"]
+                   ]
         :signing {:gpg-key "FA541587"}}}
