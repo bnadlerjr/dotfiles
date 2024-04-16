0e446cbddb25d32dd78b1eab8da2cd9fdeaf0779
Bob Nadler <bnadlerjr@gmail.com>
2024-04-16 18:43:14 -0400

    iex: Add basic benchmarking utilities

--- iex.exs
+++ iex.exs
@@ -2,18 +2,36 @@
 IEx.configure(
   colors: [
     enabled: true,
-    eval_result: [ :cyan, :bright ],
-    eval_error:  [ :light_magenta ],
+    eval_result: [:cyan, :bright],
+    eval_error: [:light_magenta]
   ],
-  default_prompt: [
-    "\r\e[38;5;220m",         # a pale gold
-    "%prefix",                # IEx context
-    "\e[38;5;112m(%counter)", # forest green expression count
-    "\e[38;5;220m>",          # gold ">"
-    "\e[0m",                  # and reset to default color
-  ] 
-  |> IO.chardata_to_string
+  default_prompt:
+    [
+      # a pale gold
+      "\r\e[38;5;220m",
+      # IEx context
+      "%prefix",
+      # forest green expression count
+      "\e[38;5;112m(%counter)",
+      # gold ">"
+      "\e[38;5;220m>",
+      # and reset to default color
+      "\e[0m"
+    ]
+    |> IO.chardata_to_string()
 )
 
-import_if_available Ecto.Query, only: [from: 2]
-import_if_available Ecto.Changeset
+import_if_available(Ecto.Query, only: [from: 2])
+import_if_available(Ecto.Changeset)
+
+defmodule Benchmark do
+  @moduledoc """
+  Super basic benchmarking utilities.
+  """
+  def measure(function) do
+    function
+    |> :timer.tc()
+    |> elem(0)
+    |> Kernel./(1_000_000)
+  end
+end
